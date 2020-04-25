48b4da270f96cb5a0d42494eb7496502fec2b280
WimAlsemgeest <wim@wimalsemgeest.nl>
2020-04-25 16:33:10 +0200

    Changed the module logging.

--- mymodule/mymodule.py
+++ mymodule/mymodule.py
@@ -1,14 +1,14 @@
 """A function for example an external file logging"""
 import logging.config
 
+
 def some_function() -> None:
     """Create some logging texts."""
     logger = logging.getLogger(__name__)
 
     print("This are messages from a function in a module.ls")
-    logging.critical("This is a critical message from a external function.")
-    logging.error("This is a error message from a external function.")
-    logging.warning("This is a warning message from a external function.")
-    logging.info("This is a info message from a external function.")
-    logging.debug("This is a debug message from a external function.")
-    
+    logger.critical("This is a critical message from a external function.")
+    logger.error("This is a error message from a external function.")
+    logger.warning("This is a warning message from a external function.")
+    logger.info("This is a info message from a external function.")
+    logger.debug("This is a debug message from a external function.")
